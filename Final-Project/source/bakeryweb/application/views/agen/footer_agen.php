6a200fb66cc3b7b6f5d8b4866e156900c81a2315
harysusilo50 <57900161+harysusilo50@users.noreply.github.com>
2021-01-07 23:57:13 +0700

    Add files via upload

--- Final-Project/source/bakeryweb/application/views/agen/footer_agen.php
+++ Final-Project/source/bakeryweb/application/views/agen/footer_agen.php
@@ -3,58 +3,63 @@
         <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
         <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.5.1/jquery.min.js" integrity="sha512-bLT0Qm9VnAYZDflyKcBaQ2gg0hSYNQrJ8RilYldYQ1FxQYoCLtUjuuRuZo+fjqhx/qtq/1itJ0C2ejDxltZVFg==" crossorigin="anonymous"></script>
-        
-        <script>
-        $(document).ready(function(){ 
- 
-            $('#jenisRoti').change(function(){ 
-                $.ajax({
-                    type: "POST", 
-                    url: "<?= base_url("Agen/list_roti");?>", 
-                    data: {id_jenisRoti : $('#jenisRoti').val()}, 
-                    dataType: "JSON",
-                    beforeSend: function(e) {
-                        if(e && e.overrideMimeType) {
-                                e.overrideMimeType("application/json;charset=UTF-8");
-                        }
-                    },
-                    success: function(response){ 
 
-                        $("#roti").html(response.list_roti).show();
-                    },
-                    error: function (xhr, ajaxOptions, thrownError) {
-                        alert(xhr.status + "\n" + xhr.responseText + "\n" + thrownError); 
-                    }
+        <script>
+            $(document).ready(function() {
+
+                $('#jenisRoti').change(function() {
+                    $.ajax({
+                        type: "POST",
+                        url: "<?= base_url("Agen/list_roti"); ?>",
+                        data: {
+                            id_jenisRoti: $('#jenisRoti').val()
+                        },
+                        dataType: "JSON",
+                        beforeSend: function(e) {
+                            if (e && e.overrideMimeType) {
+                                e.overrideMimeType("application/json;charset=UTF-8");
+                            }
+                        },
+                        success: function(response) {
+
+                            $("#roti").html(response.list_roti).show();
+                        },
+                        error: function(xhr, ajaxOptions, thrownError) {
+                            alert(xhr.status + "\n" + xhr.responseText + "\n" + thrownError);
+                        }
+                    });
                 });
             });
-        });
 
-    $(document).ready(function(){ 
+            $(document).ready(function() {
 
-        $('#tanggal').change(function(){ 
-            $.ajax({
-                type: "POST", 
-                url: "<?= base_url("Agen/tampil_riwayat");?>", 
-                data: {tanggal : $('#tanggal').val()}, 
-                dataType: "JSON",
-                beforeSend: function(e) {
-                    if(e && e.overrideMimeType) {
-                            e.overrideMimeType("application/json;charset=UTF-8");
-                    }
-                },
-                success: function(response){ 
+                $('#tanggal').change(function() {
+                    $.ajax({
+                        type: "POST",
+                        url: "<?= base_url("Agen/tampil_riwayat"); ?>",
+                        data: {
+                            tanggal: $('#tanggal').val()
+                        },
+                        dataType: "JSON",
+                        beforeSend: function(e) {
+                            if (e && e.overrideMimeType) {
+                                e.overrideMimeType("application/json;charset=UTF-8");
+                            }
+                        },
+                        success: function(response) {
 
-                    $("#data").html(response.data_tabel).show();
-                    $("#total").html(response.jumlah).show();
-                },
-                error: function (xhr, ajaxOptions, thrownError) {
-                    alert(xhr.status + "\n" + xhr.responseText + "\n" + thrownError); 
-                }
+                            $("#data").html(response.data_tabel).show();
+                            $("#total").html(response.jumlah).show();
+                        },
+                        error: function(xhr, ajaxOptions, thrownError) {
+                            alert(xhr.status + "\n" + xhr.responseText + "\n" + thrownError);
+                        }
+                    });
+                });
             });
-        });
-    });
-    </script>
+        </script>
 
-    
-  </body>
-</html>
+
+        </body>
+
+        </html>
